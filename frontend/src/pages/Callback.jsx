b2bb2af0cefdfd8cfade2eda3a03e98d1263c8f3
Alexander Nagy <nagyal3x@gmail.com>
2022-06-29 22:06:30 +0200

    :bulb:

--- frontend/src/pages/Callback.jsx
+++ frontend/src/pages/Callback.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect, useContext, createContext } from "react";
+import { React, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../providers/auth";
 
