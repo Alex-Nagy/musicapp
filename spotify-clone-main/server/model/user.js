beca0da592eec141114b30c2174cb0757b3c9b40
Alexander Nagy <nagyal3x@gmail.com>
2022-07-03 00:24:09 +0200

    profile post db

--- spotify-clone-main/server/model/user.js
+++ spotify-clone-main/server/model/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   artistName: { type: String },
   country: { type: String },
-  email: { type: String },
+  email: { type: String, unique: true, sparse: true },
   languages: { type: Array },
   genres: { type: Array },
 });
