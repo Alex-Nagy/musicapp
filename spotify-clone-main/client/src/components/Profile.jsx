5763791e5b280be0ea3c0bb5b8a52da9cc5ceeeb
Alexander Nagy <nagyal3x@gmail.com>
2022-07-02 22:32:22 +0200

    test

--- spotify-clone-main/client/src/components/Profile.jsx
+++ spotify-clone-main/client/src/components/Profile.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 const Profile = () => {
   return (
-    <form>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
       <h1>My Profile</h1>
       <Avatar />
-      <TextField label="Email" type="search" />
-      <TextField label="Username" type="search" />
-      <TextField label="City" type="search" />
+      <TextField size="small" label="My Artist Name" color="secondary" />
+      <TextField size="small" label="Country" />
+      <TextField size="small" label="Email" />
+      <TextField size="small" label="Languages" />
+      <TextField size="small" label="Genres" />
       <br />
-      <Button variant="contained" color="success">
+      <Button size="small" variant="contained" color="success">
         Save
       </Button>
-      <Button variant="outlined" color="error">
+      <Button size="small" variant="outlined" color="error">
         Cancel
       </Button>
-    </form>
+    </Box>
   );
 };
 
---
+++ spotify-clone-main/server/model/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  artistName: { type: String },
+  country: { type: String },
+  email: { type: String },
+  languages: { type: Array },
+  genres: { type: Array },
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
+
+/*
+todos: { type: todoSchema, default: () => [] }, // empty list is default?
+*/
