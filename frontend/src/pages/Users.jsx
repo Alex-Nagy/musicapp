fc689318ffb46655139ee5e7ec7c997b28f730ff
Alexander Nagy <nagyal3x@gmail.com>
2022-07-20 04:28:29 +0200

    users css

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import "./Users.scss";
 
 const Users = ({ spotID }) => {
   const [users, setUsers] = useState([]);
@@ -39,75 +40,80 @@ const Users = ({ spotID }) => {
 
   return (
     <div>
-      <h1 className="m-0">Users</h1>
-      <button className="btn btn-link m-0 p-0 float-right">
+      <h1 className="title-users">Users</h1>
+      <button className="onlycollab btn btn-link m-0 p-0 float-right">
         only collaborative
       </button>
-      <br />
-      {users.map((user, i) => (
-        <div
-          key={i}
-          style={{ border: "5px solid black", borderRadius: "10px", backgroundColor: "#fffd" }}
-        >
-          <p>
-            Artist name: <b>{user.artistName}</b>{" "}
-            <span className="float-right">{user.userID}</span>
-          </p>
-          <p>
-            Country: <b>{user.country}</b>
-          </p>
-          <p>
-            Email: <b>{user.email}</b>
-          </p>
-          <ul>
-            Languages:{" "}
-            <b>
-              {user.languages.map((lang, i) => (
-                <li key={i}>{lang}</li>
-              ))}
-            </b>
-          </ul>
-          <ul>
-            Genres:{" "}
-            <b>
-              {user.genres.map((genre, i) => (
-                <li key={i}>{genre}</li>
-              ))}
-            </b>
-          </ul>
-          <p>
-            Open now to Collaborate:{" "}
-            <b>
-              {user.collab ? (
-                <i style={{ color: "green" }}>YES✔</i>
-              ) : (
-                <i style={{ color: "red" }}>NO❌</i>
-              )}
-            </b>
-          </p>
-          <Button size="small" variant="outlined">
-            Send email
-          </Button>
-          <Button
-            size="small"
-            variant="outlined"
-            onClick={() =>
-              saveContact(
-                user.userID,
-                user.artistName,
-                user.country,
-                user.email,
-                user.languages,
-                user.genres,
-                user.collab,
-                user.instruments
-              )
-            }
+      <div className="collab-body">
+        <br />
+        {users.map((user, i) => (
+          <div
+            key={i}
+            className="user-box"
           >
-            Save
-          </Button>
-        </div>
-      ))}
+            <p>
+              Artist name: <b>{user.artistName}</b>{" "}
+            </p>
+              <p className="userid">{user.userID}</p>
+            <p>
+              Country: <b>{user.country}</b>
+            </p>
+            <p>
+              Email: <b>{user.email}</b>
+            </p>
+            <ul>
+              Languages:{" "}
+              <b>
+                {user.languages.map((lang, i) => (
+                  <li key={i}>{lang}</li>
+                ))}
+              </b>
+            </ul>
+            <ul>
+              Genres:{" "}
+              <b>
+                {user.genres.map((genre, i) => (
+                  <li key={i}>{genre}</li>
+                ))}
+              </b>
+            </ul>
+            <p>
+              Open now to Collaborate:{" "}
+              <b>
+                {user.collab ? (
+                  <i style={{ color: "green" }}>YES✔</i>
+                ) : (
+                  <i style={{ color: "red" }}>NO❌</i>
+                )}
+              </b>
+            </p>
+            <div>
+              <Button size="small" variant="outlined" color="secondary">
+                Send email
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                color="info"
+                onClick={() =>
+                  saveContact(
+                    user.userID,
+                    user.artistName,
+                    user.country,
+                    user.email,
+                    user.languages,
+                    user.genres,
+                    user.collab,
+                    user.instruments
+                  )
+                }
+              >
+                Save
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
